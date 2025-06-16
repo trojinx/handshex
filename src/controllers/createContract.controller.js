ff05a8d57d7f349c93e10e0dc09b159f67568806
shaaradnj <shaaradjarandikar@gmail.com>
2025-06-16 15:41:54 +0530

    remove useless logs

--- src/controllers/createContract.controller.js
+++ src/controllers/createContract.controller.js
@@ -5,20 +5,12 @@ async function createContract(req, res) {
   try {
     const { contractName, contractRecieverEmail, expiryDate, contractBody } =
       req.body;
-    console.log("Decoded JWT payload (req.user):", req.user);
-    console.log("req.user.id:", req.user.id);
-    console.log("Type of req.user.id:", typeof req.user.id);
-
-    //this is to get id of contract creator from headers
-    //note for myself: bcoz req.user.id returns just a string, and becoz of schema, only objectID is expected
-    // ObjectID is not a plain string!!!
-    //so to convert string to ObjectID, mongoose.Types.ObjectID() is used....
-    const contractMaker = new mongoose.Types.ObjectId(req.user.id);
+    const contractMaker = req.user.id;
 
     let contractReciever;
     const existingUser = await User.findOne({ email: contractRecieverEmail });
     if (existingUser) {
-      contractReciever = new mongoose.Types.ObjectId(existingUser._id);
+      contractReciever = existingUser._id;
     } else {
       return res.status(400).send("reciever user is not registered");
     }
